f41eb37c837bed4764eb3970e0db86c8cc220e91
Evan Tung <dragonejt@gmail.com>
2026-04-09 23:35:05 -0400

    fix: fix job table

--- src/iceberg_mcp_data/extractions/ingest_pypi_data.py
+++ src/iceberg_mcp_data/extractions/ingest_pypi_data.py
@@ -1,8 +1,20 @@
 from datetime import datetime, timezone, timedelta
-from databricks.sdk.runtime import spark
+from pyspark.sql import functions as F
+from databricks.sdk.runtime import spark, dbutils
+
+catalog = dbutils.widgets.get("catalog")
+schema = dbutils.widgets.get("schema")
 
 previous_day = (datetime.now(timezone.utc) - timedelta(days=1)).date()
 
 file_downloads = spark.read.table("bigquery.pypi.file_downloads")
+file_downloads = file_downloads.filter(
+    (F.col("timestamp") >= F.lit(previous_day)) & (F.col("timestamp") < F.lit(previous_day + timedelta(days=1)))
+)
 
-file_downloads.write.format("delta").mode("append").save("pypi_file_downloads")
+table = f"{catalog}.{schema}.pypi_file_downloads"
+
+if spark.catalog.tableExists(table):
+    file_downloads.writeTo(table).append()
+else:
+    file_downloads.writeTo(table).using("delta").create()
