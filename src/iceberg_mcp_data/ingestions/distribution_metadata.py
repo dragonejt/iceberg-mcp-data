b168c359326a6d6012649d378dea8f692fb06032
Evan Tung <dragonejt@gmail.com>
2026-07-21 16:54:29 -0700

    fix: change ingestion into functions

--- src/iceberg_mcp_data/ingestions/distribution_metadata.py
+++ src/iceberg_mcp_data/ingestions/distribution_metadata.py
@@ -1,37 +1,49 @@
 from base64 import b64encode
+from datetime import datetime, timezone
 
 from databricks.sdk.runtime import dbutils, spark
+from pyspark.sql import SparkSession
 
 credentials = dbutils.secrets.get(scope="gcp", key="bigquery")
 credentials = b64encode(credentials.encode()).decode()
 
-query = """
-SELECT *
-FROM `bigquery-public-data.pypi.distribution_metadata`
-WHERE name = 'iceberg-mcp-server'
-AND DATE_TRUNC(DATE(upload_time), MONTH) = DATE_TRUNC(DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH), MONTH)
-"""
 
-file_downloads = (
-    spark.read.format("bigquery")
-    .option("credentials", credentials)
-    .option("query", query)
-    .option("viewsEnabled", "true")
-    .option("parentProject", "dragonejt")
-    .option("materializationProject", "dragonejt")
-    .option("materializationDataset", "databricks")
-    .load()
-)
+def distribution_metadata(spark: SparkSession) -> None:
+    # Only executes on first of each month to get previous month's data
+    if datetime.now(timezone.utc).day != 1:
+        return
 
-table = "workspace.default.distribution_metadata"
+    query = """
+    SELECT *
+    FROM `bigquery-public-data.pypi.distribution_metadata`
+    WHERE name = 'iceberg-mcp-server'
+    AND DATE_TRUNC(DATE(upload_time), MONTH) = DATE_TRUNC(DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH), MONTH)
+    """
 
-if spark.catalog.tableExists(table):
-    file_downloads.writeTo(table).append()
-else:
-    (
-        file_downloads.writeTo(table)
-        .using("iceberg")
-        .tableProperty("format-version", "3")
-        .tableProperty("write.delete.mode", "merge-on-read")
-        .create()
+    file_downloads = (
+        spark.read.format("bigquery")
+        .option("credentials", credentials)
+        .option("query", query)
+        .option("viewsEnabled", "true")
+        .option("parentProject", "dragonejt")
+        .option("materializationProject", "dragonejt")
+        .option("materializationDataset", "databricks")
+        .load()
     )
+
+    table = "workspace.default.distribution_metadata"
+
+    if spark.catalog.tableExists(table):
+        file_downloads.writeTo(table).append()
+    else:
+        (
+            file_downloads.writeTo(table)
+            .using("iceberg")
+            .tableProperty("format-version", "3")
+            .tableProperty("write.delete.mode", "merge-on-read")
+            .create()
+        )
+
+
+if __name__ == "__main__":
+    distribution_metadata(spark)
--- src/iceberg_mcp_data/ingestions/file_downloads.py
+++ src/iceberg_mcp_data/ingestions/file_downloads.py
@@ -1,42 +1,44 @@
 from base64 import b64encode
-from datetime import datetime, timezone
-from sys import exit
 
 from databricks.sdk.runtime import dbutils, spark
-
-if datetime.now(timezone.utc).day != 1:
-    exit()
+from pyspark.sql import SparkSession
 
 credentials = dbutils.secrets.get(scope="gcp", key="bigquery")
 credentials = b64encode(credentials.encode()).decode()
 
-query = """
-SELECT *
-FROM `bigquery-public-data.pypi.file_downloads`
-WHERE project = "iceberg-mcp-server"
-AND DATE(timestamp) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
-"""
 
-file_downloads = (
-    spark.read.format("bigquery")
-    .option("credentials", credentials)
-    .option("query", query)
-    .option("viewsEnabled", "true")
-    .option("parentProject", "dragonejt")
-    .option("materializationProject", "dragonejt")
-    .option("materializationDataset", "databricks")
-    .load()
-)
+def file_downloads(spark: SparkSession) -> None:
+    query = """
+    SELECT *
+    FROM `bigquery-public-data.pypi.file_downloads`
+    WHERE project = "iceberg-mcp-server"
+    AND DATE(timestamp) = DATE_SUB(CURRENT_DATE(), INTERVAL 1 DAY)
+    """
 
-table = "workspace.default.file_downloads"
-
-if spark.catalog.tableExists(table):
-    file_downloads.writeTo(table).append()
-else:
-    (
-        file_downloads.writeTo(table)
-        .using("iceberg")
-        .tableProperty("format-version", "3")
-        .tableProperty("write.delete.mode", "merge-on-read")
-        .create()
+    file_downloads = (
+        spark.read.format("bigquery")
+        .option("credentials", credentials)
+        .option("query", query)
+        .option("viewsEnabled", "true")
+        .option("parentProject", "dragonejt")
+        .option("materializationProject", "dragonejt")
+        .option("materializationDataset", "databricks")
+        .load()
     )
+
+    table = "workspace.default.file_downloads"
+
+    if spark.catalog.tableExists(table):
+        file_downloads.writeTo(table).append()
+    else:
+        (
+            file_downloads.writeTo(table)
+            .using("iceberg")
+            .tableProperty("format-version", "3")
+            .tableProperty("write.delete.mode", "merge-on-read")
+            .create()
+        )
+
+
+if __name__ == "__main__":
+    file_downloads(spark)
